480ad52d667f5c5f864102a87877db40111e49b1
shubh-37 <shubh.arya37@gmail.com>
2022-12-26 21:39:29 +0530

    added solution to exercise 2

--- ex2.js
+++ ex2.js
@@ -20,8 +20,31 @@ function output(text) {
 // **************************************
 
 function getFile(file) {
-	// what do we do here?
+	var text, fn;
+	fakeAjax(file, function(response){
+		if (fn) fn(response);
+		else text = response;
+	});
+
+	return function(cb){
+		if (text) cb(text);
+		else fn = cb;
+	}
 }
 
 // request all files at once in "parallel"
-// ???
+var th1 = getFile("file1");
+var th2 = getFile("file2");
+var th3 = getFile("file3");
+
+th1(getOutput);
+function getOutput(text1){
+	output(text1);
+	function th2(text2){
+		output(text2);
+		function th3(text3){
+			output(text3);
+			output("completed!");
+		}
+	}
+}
